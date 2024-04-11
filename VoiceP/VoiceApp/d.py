7ed60e58ce86cf9d4397018b5ff75ace0f7c9471
erdenebayr <erdenebayr.eba333@gmail.com>
2024-04-11 15:43:16 +0800

    test2

---
+++ VoiceP/VoiceApp/d.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+date = datetime.now()
+time= date.strftime("%H:%M")
+print(time)
--- VoiceP/VoiceApp/views.py
+++ VoiceP/VoiceApp/views.py
@@ -1,6 +1,37 @@
 from django.shortcuts import render
+from datetime import datetime
+import requests
+from playsound import playsound
 
-# Create your views here.
+def synthesize(text):
+    url = "https://api.chimege.com/v1.2/synthesize"
+    headers = {
+        'Content-Type': 'plain/text',
+        'Token': 'API_TOKEN',
+    }
+
+    r = requests.post(
+        url, data=text.encode('utf-8'), headers=headers)
+
+    with open("output.wav", 'wb') as out:
+        out.write(r.content)
+
+
+print(synthesize('Сайн байна уу'))
+
 
 def index(request):
-    return render(request, 'index.html')
+    answer = ''
+    value = ''
+    if request.method == 'POST':
+        value = request.POST.get('text')
+        if value == 'сайн уу':
+            answer = 'сайн байна уу'
+            synthesize(answer)
+            playsound('output.wav')
+
+    context = {
+        'questions':value,
+        'answer': answer
+    }
+    return render(request, 'index.html', context=context)
