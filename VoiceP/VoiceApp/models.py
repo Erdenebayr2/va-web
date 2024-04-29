489d2314146fdf32b180a23823e2e0d153038d9d
Erdenebayr2 <94421082+Erdenebayr2@users.noreply.github.com>
2024-04-29 16:57:20 +0800

    models test1

--- VoiceP/VoiceApp/models.py
+++ VoiceP/VoiceApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Chat(models.Model):
+    userchat = models.CharField(max_length=200)
+    aichat = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return f"Chat {self}"
+    
+class Group(models.Model):
+    userchat = models.CharField(max_length=200)
+    aichat = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return f"Chat {self}"
--- VoiceP/VoiceApp/views.py
+++ VoiceP/VoiceApp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+<<<<<<< Updated upstream
 from datetime import datetime
 import requests
 from playsound import playsound
+=======
+import requests, datetime
+
+>>>>>>> Stashed changes
 
 def synthesize(text):
     url = "https://api.chimege.com/v1.2/synthesize"
@@ -21,6 +26,7 @@ print(synthesize('Сайн байна уу'))
 
 
 def index(request):
+<<<<<<< Updated upstream
     answer = ''
     value = ''
     if request.method == 'POST':
@@ -35,3 +41,19 @@ def index(request):
         'answer': answer
     }
     return render(request, 'index.html', context=context)
+=======
+    context = {}
+    current_date = datetime.date.today()
+    formatted_date = current_date.strftime("%A")
+    if request.method=='POST':
+        chat = request.POST.get('chat')
+        if 'Өнөөдөр' in chat:
+            context['chat'] = f'Өнөөдөр {formatted_date} гараг.'
+        if 'Чи юу хийж чадах вэ' in chat:
+            context['chat'] = 'Би таньд цаг агаар хэлэх, өдрийн тэмдэглэл хөтлөх, зурхай хэлэх, цаг хэлэх зэргийг хийж чадна.'
+
+        else:
+            pass
+
+    return render(request, 'index.html',context=context)
+>>>>>>> Stashed changes
